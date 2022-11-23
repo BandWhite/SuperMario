982f52dba60882e7a980ff7254592d0e1bc2646a
shaoyan <ssyy1996>
2022-11-23 18:36:17 +0800

    init

---
+++ Source/SuperMario/Player/MarioCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MarioCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERMARIO_API AMarioCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SuperMario/SuperMarioGameModeBase.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperMarioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERMARIO_API ASuperMarioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASuperMarioGameModeBase();
+};
